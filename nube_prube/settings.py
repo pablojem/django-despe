c4200d1f9bf03b06e08b5faa84b9315822561aa0
pablojem <razahumano@gmail.com>
2025-04-14 16:16:20 -0300

    csrftoken

--- nube_prube/settings.py
+++ nube_prube/settings.py
@@ -139,3 +139,6 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = ['http://*','https://web-production-47b9.up.railway.app']
